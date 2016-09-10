20aec8616ebf2b67291e921f9280936ffc47e004
Vincent ROULLIER <vincent.roullier@gmail.com>
2016-09-10 23:04:15 +0200

    Create a first view and url

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
+#-*- coding: utf-8 -*-
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    """ Exemple de page HTML, non valide pour que l'exemple soit concis """
+
+    text = u"""<h1>Bienvenue sur mon blog !</h1>
+
+                  <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>"""
+
+    return HttpResponse(text)
--- crepes_bretonnes/urls.py
+++ crepes_bretonnes/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from blog import views
 
 urlpatterns = [
+    url(r'^accueil$', views.home),
     url(r'^admin/', admin.site.urls),
 ]
